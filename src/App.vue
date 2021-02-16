38f74b78e1464ae102a0ed3ebf873c29efb76e33
manthan kyada <47109996+manthan37@users.noreply.github.com>
2021-02-16 12:48:56 +0530

    basic styling done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
+  <the-header></the-header>
   <router-view/>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
 
 <style>
 #app {
@@ -14,17 +24,7 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body{
+  margin: 0;
 }
 </style>
---
+++ src/views/coaches/CoachesList.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>FILTER</section>
+  <section>LIST</section>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
